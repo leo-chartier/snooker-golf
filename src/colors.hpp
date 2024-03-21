3178078127e5eb4d95b3b83433f5dd632b724cc2
leo-chartier <leo.chartier@algosup.com>
2024-03-21 12:27:16 +0100

    Add colors definitions

---
+++ src/colors.hpp
@@ -0,0 +1,11 @@
+#ifndef GAME_COLORS
+#define GAME_COLORS
+#include <SFML/Graphics.hpp>
+
+const sf::Color RAILS_COLOR = sf::Color(64, 32, 32);
+const sf::Color CLOTH_COLOR = sf::Color::Green;
+const sf::Color POCKET_COLOR = sf::Color::Black;
+const sf::Color CUE_BALL_COLOR = sf::Color::White;
+const sf::Color BALL_COLOR = sf::Color::Red;
+
+#endif
