51cf8506e5c6998bc10f047e3da38450dd07348f
Malo-Archimbaud <malo.archimbaud@algosup.com>
2024-04-03 09:56:33 +0200

    PLace the ball at the right positions

--- src/main.cpp
+++ src/main.cpp
@@ -30,64 +30,33 @@ int main()
 
     // The list of pockets
     std::vector<Pocket> pocketList = {Pocket(Vector2f(12, 12), 2), // TEMP : Set the hole radius to 2 for now but should be tied to mouth size
-                            //Pocket(Vector2f(12, 8), 2),
-                            //Pocket(Vector2f(16, 8), 2),
-                            //Pocket(Vector2f(20, 8), 2),
-                            //Pocket(Vector2f(24, 8), 2),
-                            //Pocket(Vector2f(28, 8), 2),
-                            //Pocket(Vector2f(32, 8), 2),
-                            //Pocket(Vector2f(36, 8), 2),
-                            //Pocket(Vector2f(40, 8), 2),
-                            //Pocket(Vector2f(44, 8), 2),
-                            //Pocket(Vector2f(48, 8), 2),
-                            //Pocket(Vector2f(52, 8), 2),
-                            //Pocket(Vector2f(56, 8), 2),
-                            //Pocket(Vector2f(60, 8), 2),
-                            //Pocket(Vector2f(64, 8), 2),
-                            //Pocket(Vector2f(68, 8), 2),
-                            //Pocket(Vector2f(72, 8), 2),
-                            //Pocket(Vector2f(76, 8), 2),
-                            //Pocket(Vector2f(80, 8), 2),
-                            //Pocket(Vector2f(80, 12), 2),
-                            //Pocket(Vector2f(80, 16), 2),
-                            //Pocket(Vector2f(80, 20), 2),
-                            //Pocket(Vector2f(80, 24), 2),
-                            //Pocket(Vector2f(80, 28), 2),
-                            //Pocket(Vector2f(80, 32), 2),
-                            //Pocket(Vector2f(80, 36), 2),
-                            //Pocket(Vector2f(76, 36), 2),
-                            //Pocket(Vector2f(72, 36), 2),
-                            //Pocket(Vector2f(68, 36), 2),
-                            //Pocket(Vector2f(64, 36), 2),
-                            //Pocket(Vector2f(60, 36), 2),
-                            //Pocket(Vector2f(56, 36), 2),
-                            //Pocket(Vector2f(52, 36), 2),
-                            //Pocket(Vector2f(48, 36), 2),
-                            //Pocket(Vector2f(44, 36), 2),
-                            //Pocket(Vector2f(40, 36), 2),
-                            //Pocket(Vector2f(36, 36), 2),
-                            //Pocket(Vector2f(32, 36), 2),
-                            //Pocket(Vector2f(28, 36), 2),
-                            //Pocket(Vector2f(24, 36), 2),
-                            //Pocket(Vector2f(20, 36), 2),
-                            //Pocket(Vector2f(16, 36), 2),
-                            //Pocket(Vector2f(12, 36), 2),
-                            //Pocket(Vector2f(12, 32), 2),
-                            //Pocket(Vector2f(12, 28), 2),
-                            //Pocket(Vector2f(12, 24), 2),
-                            //Pocket(Vector2f(12, 20), 2),
-                            //Pocket(Vector2f(12, 16), 2),
-                            //Pocket(Vector2f(12, 12), 2),
                             };
 
     CueBall cueBall = CueBall(Vector2f(CLASSIC_WIDTH / 4, CLASSIC_HEIGHT / 2), BALL_RADIUS, CUE_BALL_COLOR);
 
     vector<Ball> ballsList;
-    for (int i = 0; i < 3; i++) {
-        Ball ball = Ball(Vector2f(CLASSIC_WIDTH * 3 / 4 + i * 2 * BALL_RADIUS, CLASSIC_HEIGHT / 2), BALL_RADIUS, BALL_COLOR);
+    for (int i = 0; i < 15; i++) {
+        Ball ball = Ball(Vector2f(0, 0), BALL_RADIUS, BALL_COLOR);
         ballsList.push_back(ball);
     }
-    // ballsList.push_back(cueBall);
+
+    // Set the position of the balls
+    ballsList[0].Position = Vector2f(CLASSIC_WIDTH * 3 / 4, CLASSIC_HEIGHT / 2);
+    ballsList[1].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 2 * BALL_RADIUS, CLASSIC_HEIGHT / 2 + BALL_RADIUS);
+    ballsList[2].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 2 * BALL_RADIUS, CLASSIC_HEIGHT / 2 - BALL_RADIUS);
+    ballsList[3].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 4 * BALL_RADIUS, CLASSIC_HEIGHT / 2 + 2 * BALL_RADIUS);
+    ballsList[4].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 4 * BALL_RADIUS, CLASSIC_HEIGHT / 2);
+    ballsList[5].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 4 * BALL_RADIUS, CLASSIC_HEIGHT / 2 - 2 * BALL_RADIUS);
+    ballsList[6].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 6 * BALL_RADIUS, CLASSIC_HEIGHT / 2 + 3 * BALL_RADIUS);
+    ballsList[7].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 6 * BALL_RADIUS, CLASSIC_HEIGHT / 2 + BALL_RADIUS);
+    ballsList[8].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 6 * BALL_RADIUS, CLASSIC_HEIGHT / 2 - BALL_RADIUS);
+    ballsList[9].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 6 * BALL_RADIUS, CLASSIC_HEIGHT / 2 - 3 * BALL_RADIUS);
+    ballsList[10].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 8 * BALL_RADIUS, CLASSIC_HEIGHT / 2 + 4 * BALL_RADIUS);
+    ballsList[11].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 8 * BALL_RADIUS, CLASSIC_HEIGHT / 2 + 2 * BALL_RADIUS);
+    ballsList[12].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 8 * BALL_RADIUS, CLASSIC_HEIGHT / 2);
+    ballsList[13].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 8 * BALL_RADIUS, CLASSIC_HEIGHT / 2 - 2 * BALL_RADIUS);
+    ballsList[14].Position = Vector2f(CLASSIC_WIDTH * 3 / 4 + 8 * BALL_RADIUS, CLASSIC_HEIGHT / 2 - 4 * BALL_RADIUS);
+
 
     Cue cue = Cue(cueBall.Position, Vector2f(0,0), 0, 0);
 
