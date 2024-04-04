fcb671f6ab697bea5676a72a40dfd5ee56693c13
leo-chartier <leo.chartier@algosup.com>
2024-04-04 02:11:53 +0200

    Fix cloth drawing for non-simple shapes

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,43 @@ int main()
     VideoMode desktop = sf::VideoMode::getDesktopMode();
     window.setPosition(Vector2i(desktop.width/2 - window.getSize().x/2, desktop.height/2 - window.getSize().y/2));
 
-    Vector2f shape[] = {
-        Vector2f(),
-        Vector2f(CLASSIC_WIDTH / 2, 0),
-        Vector2f(CLASSIC_WIDTH / 2, CLASSIC_HEIGHT / 4),
-        Vector2f(CLASSIC_WIDTH * 2, CLASSIC_HEIGHT / 4),
-        Vector2f(CLASSIC_WIDTH * 2, CLASSIC_HEIGHT),
-        Vector2f(0, CLASSIC_HEIGHT),
+    Vector2f vertices[] = {
+        Vector2f(  0,    144),
+        Vector2f(192,    144),
+        Vector2f(192,     88.58),
+        Vector2f(182.06,  81.94),
+        Vector2f(171.65,  66.37),
+        Vector2f(168,     48),
+        Vector2f(171.65,  29.63),
+        Vector2f(182.06,  14.06),
+        Vector2f(197.63,   3.65),
+        Vector2f(216,      0),
+        Vector2f(234.37,   3.65),
+        Vector2f(249.94,  14.06),
+        Vector2f(260.35,  29.63),
+        Vector2f(264,     48),
+        Vector2f(260.35,  66.37),
+        Vector2f(249.94,  81.94),
+        Vector2f(240,     88.58),
+        Vector2f(240,    144),
+        Vector2f(264,    144),
+        Vector2f(291.55, 149.48),
+        Vector2f(314.91, 165.09),
+        Vector2f(330.52, 188.45),
+        Vector2f(336,    216),
+        Vector2f(330.52, 243.56),
+        Vector2f(314.91, 266.91),
+        Vector2f(291.55, 282.52),
+        Vector2f(264,    288),
+        Vector2f(236.44, 282.52),
+        Vector2f(213.09, 266.91),
+        Vector2f(197.48, 243.56),
+        Vector2f(192,    216),
+        Vector2f(192,    192),
+        Vector2f(  0,    192),
     };
-    Table table = Table(shape, sizeof(shape) / sizeof(Vector2f));
+    size_t nVertices = sizeof(vertices) / sizeof(Vector2f);
+    Table table = Table(vertices, nVertices);
     initializeWindowPosition(&window, table);
 
     CueBall cueBall = CueBall(Vector2f(CLASSIC_WIDTH / 4, CLASSIC_HEIGHT / 2), BALL_RADIUS, CUE_BALL_COLOR);
--- src/table.cpp
+++ src/table.cpp
@@ -7,71 +7,93 @@
 
 using namespace sf;
 
-size_t findFollowing(size_t index, uint64_t mask, size_t length, int direction) {
-    for (int offset = 1; offset < length; offset++) {
-        size_t newIndex = (index + offset * direction);
-        if (newIndex < 0) newIndex += length;
-        if (mask & (1 << newIndex)) {
-            return newIndex;
-        }
+float cross(const Vector2f a, const Vector2f b, const Vector2f c) {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+}
+
+size_t getPrevious(uint64_t done, size_t index, size_t n) {
+    size_t prev = index;
+    do {
+        prev = (prev - 1 + n) % n;
+    } while (done & (1 << prev));
+    return prev;
+}
+
+size_t getNext(uint64_t done, size_t index, size_t n) {
+    size_t next = index;
+    do {
+        next = (next + 1) % n;
+    } while (done & (1 << next));
+    return next;
+}
+
+bool isEar(const std::vector<Vector2f>& polygon, size_t prev, size_t index, size_t next) {
+    return cross(polygon[prev], polygon[index], polygon[next]) > 0;
+}
+
+void triangulate(const std::vector<Vector2f>& polygon, ConvexShape* triangles) {
+    // TODO: Allow for holes
+    int n = polygon.size();
+    if (n < 3 || n > 64) return;
+
+    size_t prev, next;
+
+    uint64_t done = 0;
+    uint64_t ears = 0;
+    for (size_t index = 0; index < n; index++) {
+        prev = getPrevious(done, index, n);
+        next = getNext(done, index, n);
+        ears |= isEar(polygon, prev, index, next) << index;
     }
-    return index;
+
+    int remainingTriangles = n - 2;
+    size_t triangleIndex = 0;
+    size_t index = 0;
+    while (remainingTriangles > 0) {
+        uint64_t mask = 1 << index;
+        if (ears & mask) {
+            prev = getPrevious(done, index, n);
+            next = getNext(done, index, n);
+
+            // Output triangle indices
+            triangles[triangleIndex].setPoint(0, polygon[prev]);
+            triangles[triangleIndex].setPoint(1, polygon[index]);
+            triangles[triangleIndex].setPoint(2, polygon[next]);
+            triangleIndex++;
+
+            // Remove the ear
+            ears &= ~mask;
+            done |= mask;
+            remainingTriangles--;
+
+            // Update neighbors
+            ears &= ~((1 << prev) | (1 << next));
+            ears |= isEar(polygon, getPrevious(done, prev, n), prev, index) << prev;
+            ears |= isEar(polygon, index, next, getNext(done, next, n)) << next;
+        }
+        index = (index + 1) % n;
+    }
 }
 
-bool isConvex(Vector2f v0, Vector2f v1, Vector2f v2) {
-    Vector2f e1 = v1 - v0;
-    Vector2f e2 = v2 - v1;
-    float crossProduct = (e1.x * e2.y) - (e1.y * e2.x);
-    return crossProduct > 0;
-}
-
-Table::Table(Vector2f shapeArray[], size_t length) {
-    if (length < 3) return; // TODO: Error: Needs at least 3 points
-    if (length > 64) return; // TODO: Error: Implementation need at most 64 points
+Table::Table(Vector2f pointsArray[], size_t length) {
+    if (length < 3 || length > 64) perror("Must have between 3 and 64 vertices");
     this->nPoints = length;
-    this->points = shapeArray;
+    this->points = pointsArray;
     this->nTriangles = length - 2;
     this->triangles = new ConvexShape[nTriangles];
 
-    // Bit mask of the remaining edges to be clipped
-    uint64_t remaining = (1 << length) - 1;
-    for (int iteration = 0; iteration < nTriangles; iteration++) {
-        Vector2f vertex, previousVertex, nextVertex;
-        uint64_t vertexMask;
-        // Find a valid ear (three consecutive points in clockwise order)
-        for (size_t vertexIndex = 0; vertexIndex < length; vertexIndex++) {
-            vertexMask = (1 << vertexIndex);
-            if (~remaining & vertexMask) continue;
-            size_t previousIndex = findFollowing(vertexIndex, remaining, length, -1);
-            size_t nextIndex = findFollowing(vertexIndex, remaining, length, 1);
-            previousVertex = shapeArray[previousIndex];
-            vertex = shapeArray[vertexIndex];
-            nextVertex = shapeArray[nextIndex];
-            if (isConvex(previousVertex, vertex, nextVertex)) break;
-        }
-        // Add the triangle
+    std::vector<Vector2f> points(pointsArray, pointsArray + length);
+    for (size_t i = 0; i < nTriangles; i++) {
         ConvexShape triangle(3);
-        triangle.setPoint(0, previousVertex);
-        triangle.setPoint(1, vertex);
-        triangle.setPoint(2, nextVertex);
-        // triangle.setFillColor(CLOTH_COLOR);
-        triangle.setFillColor(Color(std::rand() % 255, std::rand() % 255, std::rand() % 255));
-        this->triangles[iteration] = triangle;
-        remaining &= ~vertexMask;
+        triangle.setFillColor(CLOTH_COLOR);
+        triangles[i] = triangle;
     }
+    triangulate(points, triangles);
 }
 
 void Table::Draw(RenderWindow* window) {
-    // TODO: Dynamic display with the shape
-
     // Draw the rails
-    RectangleShape rails = RectangleShape(Vector2f(
-        CLASSIC_WIDTH + 2 * CLASSIC_OUTER_MARGIN,
-        CLASSIC_HEIGHT + 2 * CLASSIC_OUTER_MARGIN
-    ));
-    rails.move(Vector2f(-CLASSIC_OUTER_MARGIN, -CLASSIC_OUTER_MARGIN));
-    rails.setFillColor(RAILS_COLOR);
-    window->draw(rails);
+    // TODO
 
     // Draw the cloth
     for (size_t index = 0; index < nTriangles; index++) {
